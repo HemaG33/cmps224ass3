aad4eacd344b8954b9cf8bfa80d79244728fc0cc
Hemendra Ghosn <hrh10@mail.aub.edu>
2024-02-12 11:27:19 +0200

    changes

--- kernel.cu
+++ kernel.cu
@@ -46,30 +46,6 @@ __global__ void mm_tiled_kernel(float* A, float* B, float* C, unsigned int M, un
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
 void mm_gpu(float* A, float* B, float* C, unsigned int M, unsigned int N, unsigned int K) {
 
     Timer timer;
