623c05501634ba0f89244c1398c2792d843781ef
Hemendra Ghosn <hrh10@mail.aub.edu>
2024-02-13 16:45:15 +0200

    changes

--- kernel.cu
+++ kernel.cu
@@ -82,7 +82,7 @@ void mm_gpu(float* A, float* B, float* C, unsigned int M, unsigned int N, unsign
     // Call kernel
     startTime(&timer);
 
-	dim3 numThreadsPerBlock(128, 128);
+	dim3 numThreadsPerBlock(32, 32);
 	dim3 numBlocks((N + numThreadsPerBlock.x - 1)/numThreadsPerBlock.x,
 	(M + numThreadsPerBlock.y - 1)/numThreadsPerBlock.y);
 	mm_tiled_kernel <<< numBlocks, numThreadsPerBlock >>> (A_d, B_d, C_d, M, N, K);
